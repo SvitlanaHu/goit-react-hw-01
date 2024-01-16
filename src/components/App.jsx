1da95494dba71161dc9a67451641d7da873dea28
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-01-16 12:53:49 +0200

    update styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,11 @@ import { FriendList } from './FriendList';
 import friendsData from './friends.json';
 import {TransactionHistory} from './TransactionHistory'
 import transactionsData from './transactions.json'
+import styles from './App.module.css'
 
 export const App = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Profile items={userData}
       />
       <FriendList items={friendsData} />
